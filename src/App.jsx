cd2bbdfd20289b383190a4ffe11a14d0afebcefc
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-12-31 12:54:31 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+import {Home, About, Projects, Contact} from './pages';
 
 
 const App = () => {
   return (
-    <div>    <h1 className="text-3xl font-bold underline text-blue-500">
-    Hello world!
-  </h1>
-  </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+
+      </Router>
+    </main>
   )
 }
 
-export default App
+export default App;
