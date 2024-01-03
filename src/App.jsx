fcaa52e29d0b6eaa9a0ed2aa9b6f66fdd54c8c6a
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2024-01-02 23:47:31 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,19 @@ import {Home, About, Projects, Experience} from './pages';
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
+    <main className="bg-slate-300/20">
       <Router>
-        <Navbar />
+        <Navbar/>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route
-            path='/*'
-            element={
-              <>
-                <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
-          </Routes>
-   
-              </>
-            }
-          />
         </Routes>
+
       </Router>
     </main>
-  );
-};
-
+  )
+}
 
 export default App;
